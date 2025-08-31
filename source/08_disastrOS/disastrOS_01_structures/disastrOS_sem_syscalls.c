761561a43c4fa0439f37cc51848a90127e295026
DavideFerrante <70373082+ferry-ai@users.noreply.github.com>
2025-08-31 20:41:28 +0000

    Aggiornamenti: -Implementate le syscall semWait e semPost

--- source/08_disastrOS/disastrOS_01_structures/disastrOS_sem_syscalls.c
+++ source/08_disastrOS/disastrOS_01_structures/disastrOS_sem_syscalls.c
@@ -71,11 +71,69 @@ void internal_semClose(){
 }
 
 void internal_semWait(){
-  running->syscall_retvalue = DSOS_ESYSCALL_NOT_IMPLEMENTED;
-  return;
+  int fd = running->syscall_args[0];
+
+  SemDescriptor* des = SemDescriptor_byFd(running, fd);
+  if (!des) {
+    running->syscall_retvalue = DSOS_ESYSCALL_ARGUMENT_OUT_OF_BOUNDS;
+    return;
+  }
+
+  Sem* sem = des->sem;
+
+  sem->count--;
+
+  if (sem->count < 0) {
+    SemDescriptorPtr* des_ptr = SemDescriptorPtr_findByDescriptor(&sem->descriptors, des);
+    if (des_ptr) {
+      List_detach(&sem->descriptors, (ListItem*) des_ptr);
+      List_insert(&sem->waiting_list, sem->waiting_list.last, (ListItem*) des_ptr);
+    }
+
+    
+    running->status = Waiting;
+    List_insert(&waiting_list, waiting_list.last, (ListItem*) running);
+
+    PCB* next_running = (PCB*) List_detach(&ready_list, ready_list.first);
+    if (next_running)
+      next_running->status = Running;
+    running = next_running;
+    return;
+  }
+
+  running->syscall_retvalue = 0;
 }
 
 void internal_semPost(){
-  running->syscall_retvalue = DSOS_ESYSCALL_NOT_IMPLEMENTED;
-  return;
+  int fd = running->syscall_args[0];
+
+  SemDescriptor* des = SemDescriptor_byFd(running, fd);
+  if (!des) {
+    running->syscall_retvalue = DSOS_ESYSCALL_ARGUMENT_OUT_OF_BOUNDS;
+    return;
+  }
+
+  Sem* sem = des->sem;
+
+  // release a token on the semaphore
+  sem->count++;
+
+  // if there are waiting processes wake up one
+  if (sem->count <= 0 && sem->waiting_list.first) {
+    // get first waiting descriptor
+    SemDescriptorPtr* des_ptr = (SemDescriptorPtr*) List_detach(&sem->waiting_list, sem->waiting_list.first);
+    if (des_ptr) {
+      // reinsert descriptor in active list
+      List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) des_ptr);
+
+      // move the corresponding process to ready state
+      PCB* pcb = des_ptr->descriptor->pcb;
+      List_detach(&waiting_list, (ListItem*) pcb);
+      pcb->status = Ready;
+      pcb->syscall_retvalue = 0;
+      List_insert(&ready_list, ready_list.last, (ListItem*) pcb);
+    }
+  }
+
+  running->syscall_retvalue = 0;
 }
