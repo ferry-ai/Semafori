19d33aa3849bf5a9dfc0cb494535faee41eb0355
DavideFerrante <70373082+ferry-ai@users.noreply.github.com>
2025-09-19 21:37:11 +0000

    Aggiornamento: implementato prod/cons, introdotto stoptoken

--- source/08_disastrOS/disastrOS_02_processes/prodcons_demo.c
+++ source/08_disastrOS/disastrOS_02_processes/prodcons_demo.c
@@ -8,7 +8,8 @@
 #define BUF_SIZE        8
 #define N_PRODUCERS     3
 #define N_CONSUMERS     2
-#define ITEMS_PER_PROD  4
+#define ITEMS_PER_PROD  10
+#define STOP_TOKEN     -1
 
 //id globali sem
 #define SEM_EMPTY_ID  1001
@@ -16,38 +17,61 @@
 #define SEM_MUTEX_ID  1003
 
 //BUFFER CONDIVISO
-int buffer[BUF_SIZE];
-int w_idx=0, r_idx=0;
+static int buffer[BUF_SIZE];
+static int w_idx=0, r_idx=0;
+static int stops_remaining = 0;
+static int producers_left = 0;
+
+static void buf_put(int v) {
+  buffer[w_idx] = v;
+  w_idx = (w_idx + 1) % BUF_SIZE;
+}
+static int buf_get() {
+  int v = buffer[r_idx];
+  r_idx = (r_idx + 1) % BUF_SIZE;
+  return v;
+}
+
+static void ds_sleep(int ticks) {
+  for (int i = 0; i < ticks; ++i) {
+    disastrOS_preempt();
+  }
+}
 
 void producer_task(void* args){
+    int id = (int)(long)args;
+
     //apertura semafori da parte del prod    
     int empty_fd = disastrOS_semOpen(SEM_EMPTY_ID, BUF_SIZE); //semaforo per dim buffer
     if(empty_fd < 0){
         printf("Errore durante la crazione empty sem");
-        disastrOS_shutdown();
+        disastrOS_exit(-1);
     }
     int fill_fd = disastrOS_semOpen(SEM_FILL_ID, 0);
     if(fill_fd < 0){
         printf("Errore durante la crazione fill sem");
-        disastrOS_shutdown();
+        disastrOS_exit(-1);
     }
     int mutex_fd = disastrOS_semOpen(SEM_MUTEX_ID, 0);
     if(mutex_fd < 0){
         printf("Errore durante la crazione mut sem");
-        disastrOS_shutdown();
+        disastrOS_exit(-1);
     }
 
     for(int i=0; i<ITEMS_PER_PROD; ++i){
         disastrOS_semWait(empty_fd);
         disastrOS_semWait(mutex_fd);
 
-        //CS
+        int item = id*100 + i;
+        buf_put(item);
+        printf("[P%d] put %d\n", id, item);
 
         disastrOS_semPost(mutex_fd);
         disastrOS_semPost(fill_fd);
 
-        
+        ds_sleep(1);
     }
+    //roba per stoppare tutto
 
 
     disastrOS_exit(0);
@@ -55,19 +79,27 @@ void producer_task(void* args){
 
 //consumatore
 void consumer_task(void* args){
+    int id = (int)args;
     int empty_fd = disastrOS_semOpen(SEM_EMPTY_ID, 0);
     int fill_fd  = disastrOS_semOpen(SEM_FILL_ID,  0);
     int mutex_fd = disastrOS_semOpen(SEM_MUTEX_ID, 0);
+    if (empty_fd < 0 || fill_fd < 0 || mutex_fd < 0) {
+    printf("  [C%d] semOpen failed\n", id);
+    disastrOS_exit(1);
+  }
 
     while(1){
         disastrOS_semWait(fill_fd);
         disastrOS_semWait(mutex_fd);
 
-        //CS
+        int item = buf_get();
 
         disastrOS_semPost(mutex_fd);
         disastrOS_semPost(empty_fd);
+
+        ds_sleep(2);
     }
+
     disastrOS_exit(0);
 }
 
@@ -90,13 +122,27 @@ void init_task(void* args){
     }
 
 
+    //inizializzazione indici
+    w_idx = r_idx = 0;
 
      // Spawna consumers e producers 
-    for (int c=0; c<5; ++c)
+    for (int c=0; c<N_CONSUMERS; ++c)
        disastrOS_spawn(consumer_task, (void*)c); //avviamo la task consumer
 
-    for (int p=0; p<5; ++p)
+    for (int p=0; p<N_PRODUCERS; ++p)
        disastrOS_spawn(producer_task, (void*)p);
+
+    int alive = N_PRODUCERS + N_CONSUMERS;
+    int retval = 0;
+    while (alive > 0) {
+       int child = disastrOS_wait(0, &retval);
+       if (child < 0)
+          break;
+       printf("[init] joined pid %d (retval=%d)\n", child, retval);
+       --alive;
+  }
+
+    disastrOS_shutdown();
 
 }
 
